da1947f5a831a07fbe60e8271355b2e5bc343756
glcalil-ufg <glcalil@discente.ufg.br>
2022-03-13 22:44:26 -0300

    Instalando modulos

--- javascript_note/src/App.js
+++ javascript_note/src/App.js
@@ -1,6 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import "./App.scss";
+import { Notification, Section } from 'rbx';
+import { Fragment } from "react";
 
-const App = () => <p>Começando</p>
-
+const App = () => (
+    <Fragment>
+        <Section>
+            <Notification color="success">
+                Olá pessoas!!
+            </Notification>
+        </Section>
+    </Fragment>
+)
 export default App;
